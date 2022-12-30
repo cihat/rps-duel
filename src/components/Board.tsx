70cd199d6996d31e6f1f504923279a5e133145be
Cihat Salik <cihatsalik1@gmail.com>
2022-12-31 01:55:42 +0300

    Add controller to settings

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SCREEN } from '../constants';
 import styles from './Board.module.css';
 // import { Player } from './Rock';
@@ -11,6 +11,14 @@ export default function Board(props: Props) {
   // const { userName, setScore, setScreen } = props;
   let ctx: CanvasRenderingContext2D | null;
   let canvas: HTMLCanvasElement;
+  const [velo, setVelo] = useState(SCREEN.VELOCITY);
+  const [corner, setCorner] = useState(SCREEN.CORNER);
+  const [rad, setRad] = useState(SCREEN.RAD);
+
+  Scissors.setVelo(velo)
+  Scissors.setCorner(corner)
+  Scissors.setRad(rad)
+
 
   useEffect(() => {
     canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
@@ -18,7 +26,7 @@ export default function Board(props: Props) {
     let scissors = [] as Scissors[];
 
     if (ctx) {
-      ctx.fillStyle = 'black';
+      ctx.fillStyle = 'blue';
       ctx.fillRect(0, 0, SCREEN.WIDTH, SCREEN.HEIGHT);
     }
 
@@ -39,7 +47,14 @@ export default function Board(props: Props) {
   })
 
   return (
-    <div className="board">
+    <div className={styles.board}>
+      <div className={styles.controller_wrapper}>
+        <div className={styles.controller}>
+          <input className='input' type="number" placeholder='velocity' onChange={({ target }) => setVelo(Number(target.value))} />
+          <input className='input' type="number" placeholder='corner' onChange={({ target }) => setCorner(Number(target.value))} />
+          <input className='input' type="number" placeholder='rad' onChange={({ target }) => setRad(Number(target.value))} />
+        </div>
+      </div>
       <canvas id='myCanvas' height={SCREEN.HEIGHT} width={SCREEN.WIDTH} className={styles.board}>
         Your browser does not support the HTML5 canvas tag.
       </canvas>
--- src/components/Scissors.tsx
+++ src/components/Scissors.tsx
@@ -2,14 +2,14 @@ import { getRandomColor } from '../utils';
 import { SCREEN } from '../constants';
 
 export class Scissors {
+  static velo: number = SCREEN.VELOCITY;
+  static corner: number = SCREEN.CORNER;
+  static rad: number = SCREEN.RAD;
   xPosition = 25
   yPosition = 25
-  velo: number = SCREEN.VELOCITY;
-  corner: number = SCREEN.CORNER;
-  rad: number = SCREEN.RAD;
 
-  moveX = Math.cos(Math.PI / 180 * this.corner) * this.velo;
-  moveY = Math.sin(Math.PI / 180 * this.corner) * this.velo;
+  moveX = Math.cos(Math.PI / 180 * Scissors.corner) * Scissors.velo;
+  moveY = Math.sin(Math.PI / 180 * Scissors.corner) * Scissors.velo;
 
   constructor(xPosition: number, yPosition: number) {
     this.xPosition = xPosition;
@@ -19,16 +19,29 @@ export class Scissors {
   draw = (ctx: CanvasRenderingContext2D) => {
     ctx.clearRect(0, 0, SCREEN.CENTER_X.get(), SCREEN.CENTER_Y.get());
 
-    if (this.xPosition > SCREEN.WIDTH - this.rad || this.xPosition < this.rad) this.moveX = -this.moveX;
-    if (this.yPosition > SCREEN.HEIGHT - this.rad || this.yPosition < this.rad) this.moveY = -this.moveY;
+    if (this.xPosition > SCREEN.WIDTH - Scissors.rad || this.xPosition < Scissors.rad) this.moveX = -this.moveX;
+    if (this.yPosition > SCREEN.HEIGHT - Scissors.rad || this.yPosition < Scissors.rad) this.moveY = -this.moveY;
 
     this.xPosition += this.moveX;
     this.yPosition += this.moveY;
 
     ctx.beginPath();
     ctx.fillStyle = getRandomColor() as string
-    ctx.arc(this.xPosition, this.yPosition, this.rad, 0, Math.PI * 2, false);
+    ctx.arc(this.xPosition, this.yPosition, Scissors.rad, 0, Math.PI * 2, false);
     ctx.fill();
     ctx.closePath();
   }
+
+  static setVelo = (velo: number) => {
+    this.velo = velo;
+  }
+
+  static setCorner = (corner: number) => {
+    this.corner = corner;
+  }
+
+  static setRad = (rad: number) => {
+    this.rad = rad;
+  }
+
 }
